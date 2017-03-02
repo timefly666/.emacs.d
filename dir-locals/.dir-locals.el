37e61ef132151a786e0d58f3b8443078ccd36ff9
Dong Jian <timeflydj@gmail.com>
2017-03-02 12:45:48 +0800

    update personal-env-cpp.el

--- dir-locals/.dir-locals.el
+++ dir-locals/.dir-locals.el
@@ -4,14 +4,14 @@
 (
  (nil . ((eval . (setq company-clang-arguments
                        `(
-
                          ,(concat "-I" (get-dir-locals-dir) "/include2/")
                          ,(concat "-I" (get-dir-locals-dir) "/include1/")
-                         ;; ,(concat (get-dir-locals-dir) "build/src/")
                          ))
                )))
 
- (nil . ((eval . (setq flycheck-clang-include-path
+ company-c-headers-path-user
+
+ (c-mode-common . ((eval . (setq flycheck-clang-include-path
                        `(
                          "/usr/include/mpich2/"
                          "/usr/local/cuda/include/"
